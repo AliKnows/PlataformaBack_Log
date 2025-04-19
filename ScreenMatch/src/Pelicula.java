0cd128828798a806caaf8e4c9c92cfb0dd0e38d4
AliKnows <130936574+AliKnows@users.noreply.github.com>
2025-04-19 16:19:25 -0500

    Actualiza Instancia de objetos

--- ScreenMatch/src/Pelicula.java
+++ ScreenMatch/src/Pelicula.java
@@ -4,7 +4,29 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaDeCalificaciones;
+    int totalEvaluaciones;
+    //creando metodos
+    /*
+    convencion de metodos: verbos en infinitivo y tambien en imperativo(mas usado)=
+    */
 
+
+    void muestraFichaTecnica(){
+        System.out.println("El nombre de la pelicula es: " + nombrePelicula);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+        System.out.println("*************");
+    }
+
+    void evalua(double calificacionPeli){
+        sumaDeCalificaciones += calificacionPeli;
+        totalEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeCalificaciones / totalEvaluaciones;
+        }
 
 
 
--- ScreenMatch/src/Principal.java
+++ ScreenMatch/src/Principal.java
@@ -1,7 +1,26 @@
 public class Principal {
     public static void main(String[] args) {
         Pelicula pelicula1 = new Pelicula();
+        pelicula1.nombrePelicula = "Matrix";
+        pelicula1.fechaDeLanzamiento = 2001;
+        pelicula1.duracionEnMinutos = 120;
+        //Llamamos a los metodos de la clase
+        pelicula1.muestraFichaTecnica();
+        pelicula1.evalua(9.5);
+        pelicula1.evalua(8);
+        System.out.println(pelicula1.sumaDeCalificaciones);
+        System.out.println(pelicula1.totalEvaluaciones);
+        System.out.println(pelicula1.calculaMedia());
 
+
+        Pelicula pelicula2 = new Pelicula();
+        pelicula2.nombrePelicula = "Mr Robot";
+        pelicula2.fechaDeLanzamiento = 2005;
+        pelicula2.duracionEnMinutos = 120;
+        pelicula2.muestraFichaTecnica();
+        pelicula2.evalua(9.8);
+
+        System.out.println(pelicula2 +" ; "+ pelicula1);
 
     }
 }
