ea9588e9d84b0a61fff538414548cef2877a5e01
AliKnows <130936574+AliKnows@users.noreply.github.com>
2025-04-19 20:47:35 -0500

    Modificadores

--- ScreenMatch/src/Principal.java
+++ ScreenMatch/src/Principal.java
@@ -1,26 +1,45 @@
+import com.AliYouKnow.screenapp.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula pelicula1 = new Pelicula();
-        pelicula1.nombrePelicula = "Matrix";
-        pelicula1.fechaDeLanzamiento = 2001;
-        pelicula1.duracionEnMinutos = 120;
+        pelicula1.setNombrePelicula("Matrix");
+        pelicula1.setFechaDeLanzamiento(2000);
+        pelicula1.setDuracionEnMinutos(120);
+
         //Llamamos a los metodos de la clase
+
         pelicula1.muestraFichaTecnica();
+
         pelicula1.evalua(9.5);
         pelicula1.evalua(8);
-        System.out.println(pelicula1.sumaDeCalificaciones);
-        System.out.println(pelicula1.totalEvaluaciones);
+        pelicula1.evalua(6);
+        System.out.println(pelicula1.getNombrePelicula());
+        System.out.println(pelicula1.getFechaDeLanzamiento());
+        System.out.println(pelicula1.getTotalEvaluaciones());
+
         System.out.println(pelicula1.calculaMedia());
 
 
-        Pelicula pelicula2 = new Pelicula();
+
+
+
+
+
+
+
+
+
+       /* com.AliYouKnow.screenapp.modelos.Pelicula pelicula2 = new com.AliYouKnow.screenapp.modelos.Pelicula();
         pelicula2.nombrePelicula = "Mr Robot";
         pelicula2.fechaDeLanzamiento = 2005;
         pelicula2.duracionEnMinutos = 120;
         pelicula2.muestraFichaTecnica();
         pelicula2.evalua(9.8);
+         System.out.println(pelicula2 +" ; "+ pelicula1);
+        */
 
-        System.out.println(pelicula2 +" ; "+ pelicula1);
+
 
     }
 }
